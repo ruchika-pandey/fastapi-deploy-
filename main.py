a8054b84111575a8fb0e956aac37fa7905f6e6c0
ruchika-pandey <ruchikapandey292@gmail.com>
2026-02-28 17:19:59 +0530

    FastAPI ML API

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI(title="ML Inference API")
+app = FastAPI()
 
-class InputData(BaseModel):
-    value: float
-
-@app.get("/")
-def home():
-    return {"message": "FastAPI ML Inference API Running"}
+class TextInput(BaseModel):
+    value: str
 
 @app.post("/predict")
-def predict(data: InputData):
-    prediction = data.value * 2   # Dummy ML logic
-    return {"prediction": prediction}
+def predict(data: TextInput):
+    text = data.value
+    return {"result": text}
